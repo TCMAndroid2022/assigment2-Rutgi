62a723521be4a6143e5bd7cd9bfb4fd3ce0e19af
CMaio <50696746+CMaio@users.noreply.github.com>
2022-05-18 13:06:41 +0200

    Primera part feta

--- Practica2FrancescRogerClaudio/app/src/main/java/cat/tecnocampus/mobileapps/practica2/francescboixaburrad/rogerrodriguezmendez/claudiomaioranaarvelo/MainActivity.java
+++ Practica2FrancescRogerClaudio/app/src/main/java/cat/tecnocampus/mobileapps/practica2/francescboixaburrad/rogerrodriguezmendez/claudiomaioranaarvelo/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tv_output;
     Button b_getRand;
+    Button b_addLetter;
     RequestQueue rqueue;
+    EditText etx_placeToWrite;
     String urlRandom = "https://palabras-aleatorias-public-api.herokuapp.com/random";
+
+    String wordResult="";
+    String wordPlayer="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         tv_output = findViewById(R.id.TV_output);
         b_getRand = findViewById(R.id.B_getword);
+        b_addLetter = findViewById(R.id.B_addLetter);
+        etx_placeToWrite = findViewById(R.id.etx_placeToWrite);
         rqueue = Volley.newRequestQueue(getApplicationContext());
 
 
@@ -40,6 +49,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 getWord();
+
+            }
+        });
+
+        b_addLetter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!etx_placeToWrite.getText().toString().equals("") ){
+                    if( !wordResult.contains(etx_placeToWrite.getText().toString())){
+                        Toast.makeText(getApplicationContext(),"No hi ha lletra",Toast.LENGTH_LONG).show();
+                    }else{
+                        addLetter(etx_placeToWrite.getText().toString().charAt(0));
+                    }
+                }
+
             }
         });
 
@@ -52,7 +76,11 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         try {
                             String strResponse = response.getJSONObject("body").getString("Word");
-                            tv_output.setText(strResponse);
+                            wordResult = strResponse;
+                            //tv_output.setText(strResponse);
+                            wordPlayer="";
+                            setWord();
+                            Log.d("Numero Letras", wordResult);
 
                         } catch (Exception ex) {
                             tv_output.setText("Json Request failed");
@@ -69,5 +97,32 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
         rqueue.add(jsonRequest);
+    }
+
+
+
+    private int numberLetters(String word){
+        return word.length();
+    }
+
+    private void setWord(){
+        for(int i = 0;i<numberLetters(wordResult);i++){
+            wordPlayer+="_";
+        }
+        tv_output.setText(wordPlayer);
+    }
+
+    private void addLetter(char letter){
+
+        StringBuilder myString = new StringBuilder(wordPlayer);
+        for(int i = 0;i<numberLetters(wordResult);i++){
+            if(wordResult.charAt(i) == letter){
+                myString.setCharAt(i,letter);
+
+            }
+        }
+        wordPlayer = myString.toString();
+        tv_output.setText(wordPlayer);
+
     }
 }
